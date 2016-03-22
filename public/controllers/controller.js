7d916c81ea0be4a65655d70e56e2cb0626e475b8
Jasdip <jasdip.chauhan@gmail.com>
2016-03-21 22:01:26 -0400

    added server functionality

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,6 +1,12 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-	console.log("test msg from controller");
+	console.log("controller test");
+
+	$http.get('/contactlist').then(function(response) {
+		console.log("I got the data i requested");
+		$scope.contactlist = response.data;
+	});
+
 }]);
 
