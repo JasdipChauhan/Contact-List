b0f7821ab3a504189a8063b4b125ee147b2a2ce7
Jasdip Chauhan <JasdipC@Jasdips-MacBook-Pro.local>
2016-04-03 01:33:24 -0400

    working on adding a mongodb database for contact information - reverted to retry

--- server.js
+++ server.js
@@ -6,23 +6,6 @@ app.use(express.static(__dirname + "/public"))
 app.get('/contactlist', function (req, res) {
 	console.log("I received a GET request");
 
-	person1 = {
-		name: "Jasdip",
-		email: "jasdip@email.com",
-		number: "(111) 111-1111"
-	};
-
-	person2 = {
-		name: "Goofie",
-		email: "goofie@email.com",
-		number: "(222) 222-2222"
-	};
-
-	person3 = {
-		name: "Person3",
-		email: "person3@email.com",
-		number: "(333) 333-3333"
-	};
 
 	var contactlist = [person1, person2, person3];
 
