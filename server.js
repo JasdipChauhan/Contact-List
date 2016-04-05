b395a99be4e77a7c8e8d9acf523c8d1692bfc4cc
Jasdip Chauhan <JasdipC@Jasdips-MacBook-Pro.local>
2016-04-04 21:37:24 -0400

    successfully retrieving user information from database

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-var express = require("express")
-var app = express()
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/contactlist', function (req, res) {
-	console.log("I received a GET request");
-	res.json(contactlist);
+  console.log('I received a GET request');
+
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
 });
 
 app.listen(3000);
